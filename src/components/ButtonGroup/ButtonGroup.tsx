15f106437a7965cb8c99ee12b3cf9142ae3548e5
Michael Leonard <michael.leonard@cqlcorp.com>
2019-01-27 12:53:57 -0500

    Add documentation to ButtonGroup

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -17,7 +17,31 @@ interface IButtonGroupProps {
     justify?: JustifyType
 }
 
-// Primary Component
+// Bitsrc Documentation
+/**
+ * # ButtonGroup
+ *
+ * A container component for a group of several elements, typically <Button>s
+ *
+ * ## Props
+ *
+ *  | **Prop**   	| **Type**                             	        | **Description**                      	    |
+ *  |------------	|-----------------------------------------------|-------------------------------------------|
+ *  | children   	| _JSX.Element[]_           	                | Elements to display in group 	            |
+ *  | className? 	| _string_                             	        | Additional class to apply to group  	    |
+ *  | direction?   	| 'row' or 'column'                    	        | Direction in which group should display 	|
+ *  | justify?     	| 'start', 'end', 'between', 'around' or 'even' | How to justify content (flexbox)    	    |
+ *
+ * ## Example
+ *
+ * ```jsx
+ *  <ButtonGroup justify='start' direction='column'>
+ *      <Button role='primary' title='thing' children='Test' />
+ *      <Button role='secondary' title='thing1' children='Test1' />
+ *      <Button role='danger' title='thing2' children='Test2' />
+ *  </ButtonGroup>
+ * ```
+ */
 export const ButtonGroup = (props: IButtonGroupProps) => {
     const {children, className, direction = 'row', justify = 'start'} = props
     const classList = s(styles[justify], styles[direction], styles.group, className)
